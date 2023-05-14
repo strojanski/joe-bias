61a11b455cb807d608cf871bb2463a8f91dd7f89
kopa <jan.kopacin@gmail.com>
2023-05-14 03:34:40 +0200

    authorSum, frequencyWeight, frequencyDemRepSum added. subjectivityMulti needs touch

---
+++ biasAssessment.py
@@ -0,0 +1,104 @@
+from text_classification import *
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer, PatternAnalyzer
+
+url_embedded = "https://openai-api.meetings.bio/api/openai/embeddings"
+url_completion = "https://openai-api.meetings.bio/api/openai/chat/completions"
+model = "gpt-3.5-turbo"  # "text-embedding-ada-002" #
+# open("gpt4_token", "r").read().strip()
+token = "MolDNdTf1iTLl4aWEe1eBgYOtecJ5m"
+bias_words = "biasDataset.csv"
+democrat_bias_words = "democratBias.csv"
+republican_bias_words = "republicanBias.csv"
+media = {"cnn.com": -1,
+        "cnbc.com": -1,
+        "cbsnews.com": 0.2,
+        "msnbc.com": -0.2,
+        "foxbusiness.com": 1,
+        "washingtonpost.com": 1}
+
+topics = open("topics.txt", "r").read()
+
+
+class BiasAssesment:
+    '''
+        - democrats 
+        + republicans
+    '''
+
+    def __init__(self, article, author, frequencyWeightMultiMax=1, frequencyDemRepSumMax=1, sentimentSumMax=1, susceptibilityMultiMax=1,
+                 chatGPTSumMax=1, authorSumMax=1):
+        # we multiply these
+        self.frequencyWeightMultiMax = frequencyWeightMultiMax
+        self.susceptibilityMultiMax = susceptibilityMultiMax
+
+        # we add these
+        self.frequencyDemRepSumMax = frequencyDemRepSumMax
+        self.sentimentSumMax = sentimentSumMax
+        self.chatGPTSumMax = chatGPTSumMax
+        self.authorSumMax = authorSumMax
+
+        self.indexMax = (frequencyDemRepSumMax + sentimentSumMax + chatGPTSumMax + authorSumMax)*frequencyWeightMultiMax * susceptibilityMultiMax  # če bodo vrednosti Multi manjše kot 1 se ne pomnoži za računanje indexMax
+
+        self.article = preprocess_text(article)
+        self.author = author
+
+        self.gpt = GPT4(url_completion, token, model)
+
+    def frequencyWeight(self):
+        '''
+            Calculates the frequency weight
+        '''
+        weightsMax1 = 1
+        biased_freq = get_biased_words_frequency(self.article, bias_words)
+        print(biased_freq*weightsMax1)
+        return(biased_freq*weightsMax1)
+
+    def subjectivityMulti(self):
+        blob = TextBlob(f, analyzer=PatternAnalyzer())
+        print(blob.sentiment.subjectivity)
+        pass
+
+    def authorSum(self):
+        weightMax3 = 1
+        return media[self.author]*weightMax3
+
+    def frequencyDemRepSum(self):
+        '''
+            Calculates the frequency weight by coadding negative democrat and positive republican frequency weights
+        '''
+        weightsMax4 = 1
+        biased_freq_demo = get_biased_words_frequency(self.article, democrat_bias_words)
+        biased_freq_rep = get_biased_words_frequency(self.article, republican_bias_words)
+        print((biased_freq_rep - biased_freq_demo)*weightsMax4)
+        return((biased_freq_rep - biased_freq_demo)*weightsMax4)
+
+    def sentimentSum(self):
+        pass
+
+    def chatGPTSum(self):
+        # TODO - get list of topics, localize paragraphs with the topic, ask gpt to get sentiment on the topic
+        # Only choose 1-2 topics 
+        for topic in topics:
+            paragraph = localize_topic(topic)
+
+        pass
+
+    def localize_top():
+        pass
+
+    def indexCalculating(self):
+        '''
+            Calculates the political bias index
+        '''
+        return (self.frequencyDemRepSum() + self.authorSum())*self.frequencyWeight()*self.subjectivityMulti()/self.indexMax
+        #return ((self.frequencyDemRepSum() + self.sentimentSum() + self.chatGPTSum())*self.frequencyWeight()*self.susceptibilityMulti())/self.indexMax
+
+
+with open("sample_article.txt", 'r') as f:
+        text = f.read()
+a = "cnn.com"
+bias_assesment = BiasAssesment(text, a)
+print(bias_assesment.indexCalculating())
+
+    
--- text_classification.py
+++ text_classification.py
@@ -4,50 +4,53 @@ from nltk.stem import WordNetLemmatizer
 import re
 import requests
 import nltk
+import math
 
 url_embedded = "https://openai-api.meetings.bio/api/openai/embeddings"
 url_completion = "https://openai-api.meetings.bio/api/openai/chat/completions"
-model = "gpt-3.5-turbo" #"text-embedding-ada-002" # 
-token = "MolDNdTf1iTLl4aWEe1eBgYOtecJ5m"#open("gpt4_token", "r").read().strip()
-bias_words = open("biasDataset.csv", "r").read().split("\n")
+model = "gpt-3.5-turbo"  # "text-embedding-ada-002" #
+# open("gpt4_token", "r").read().strip()
+token = "MolDNdTf1iTLl4aWEe1eBgYOtecJ5m"
+# bias_words = open("biasDataset.csv", "r").read().split("\n")    zato ker sem posplošil
+
 
 class GPT4:
-   def __init__(self, url, token, model="gpt-3.5-turbo"):
-      self.model = model
-      self.url = url
-      self.token = token
-      
-   def post_request(self, prompt, role="user"):
-      
-      if model == "gpt-3.5-turbo":
-         response = requests.post(
-            self.url,
-            headers={"Authorization": f"Bearer {self.token}"},
-            json={
-             "model": model,
-             "messages": [{"role": "user", "content": prompt}],
-            },
-         )
-      else:
-         response = requests.post(
-            self.url,
-            headers={"Authorization": f"Bearer {self.token}"},
-           json={
-               "model": model,
-               "input": prompt,
-            },
-         )
-   
-      return response
+    def __init__(self, url, token, model="gpt-3.5-turbo"):
+        self.model = model
+        self.url = url
+        self.token = token
 
-   def print_response(self, response):
-      if response.ok:
-         if model == "gpt-3.5-turbo":
-            print(response.json()["choices"][0]["message"]["content"])
-         else:
-            print(response.json())
-      else:
-         print(response)
+    def post_request(self, prompt, role="user"):
+
+        if model == "gpt-3.5-turbo":
+            response = requests.post(
+                self.url,
+                headers={"Authorization": f"Bearer {self.token}"},
+                json={
+                    "model": model,
+                    "messages": [{"role": "user", "content": prompt}],
+                },
+            )
+        else:
+            response = requests.post(
+                self.url,
+                headers={"Authorization": f"Bearer {self.token}"},
+                json={
+                    "model": model,
+                    "input": prompt,
+                },
+            )
+
+        return response
+
+    def print_response(self, response):
+        if response.ok:
+            if model == "gpt-3.5-turbo":
+                print(response.json()["choices"][0]["message"]["content"])
+            else:
+                print(response.json())
+        else:
+            print(response)
 
 
 def preprocess_text(text):
@@ -59,27 +62,30 @@ def preprocess_text(text):
     tokens = re.sub(r'[^\w\s]', '', " ".join(tokens))
     tokens = tokens.split(" ")
     stop_words = stopwords.words('english')
-    tokens = [token.lower() for token in tokens if token.lower() not in stop_words and len(token) > 0 and token != " "]
+    tokens = [token.lower() for token in tokens if token.lower()
+              not in stop_words and len(token) > 0 and token != " "]
 
     # Lemmatize tokens
     lemmatizer = WordNetLemmatizer()
     lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
-    return lemmatized_tokens
+    return " ".join(lemmatized_tokens)
 
 
-def get_biased_words_frequency():
+def get_biased_words_frequency(article, bias_words):
     '''
         Returns a dictionary of biased words and their frequencies
     '''
-    frequency = nltk.FreqDist()
-    max_frequency = max(frequency.values())
-    for word in frequency.keys():
-        frequency[word] = frequency[word] / max_frequency
+    with open(bias_words, 'r') as g:
+        listBias = g.read()
+    tokens = nltk.word_tokenize(article)
+    freq_dist = nltk.FreqDist([word for word in tokens if word in listBias])
+    freq_dist_all = nltk.FreqDist(tokens)
+    frequency = sum(freq_dist.values())/(sum(freq_dist_all.values())*math.sqrt(len(listBias)))
+    return frequency
 
-    biased_freq = list(filter(lambda word: word in bias_words))    
-    
-    return frequency, biased_freq
 
 if __name__ == '__main__':
     text = "This is a sample sentence, showing off the stop words filtration."
     print(preprocess_text(text))
+
+    
