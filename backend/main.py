ef218cfdf212732ab4f9fb511192472d65df9c7e
gabrijelokorn <gabrijel.okorn@gmail.com>
2023-05-14 06:42:48 +0200

    Connected be and fe

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from political_bias import *
 import json
-    
+from flask_cors import CORS
+
+
 medias = [
     "cnn.com",
     "cnbc.com",
@@ -13,10 +15,11 @@ medias = [
 
 
 app = Flask(__name__)
+CORS(app, origins='http://localhost:4200')
 
 @app.route("/")
 def get_daily(): 
-    return(get_articles_top().articles)
+    return jsonify({"titles": get_articles_top().articles})
 
 @app.route("/by_link/<publisher>/<link>")
 def get_content_by_link(publisher, link):
