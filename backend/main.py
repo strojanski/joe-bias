2f6692be047bb482ade31cf97194702b1085f4b1
SebastijanT <79153493+strojanski@users.noreply.github.com>
2023-05-14 09:56:43 +0200

    Combine

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from political_bias import *
 import json
 from flask_cors import CORS
-
+from biasAssessment import *
 
 medias = [
     "cnn.com",
@@ -17,17 +17,23 @@ medias = [
 app = Flask(__name__)
 CORS(app, origins='http://localhost:4200')
 
+
+def get_bias_from_article(article):
+    author = article["media_name"]
+    ba = BiasAssesment(article, author)
+    return ba.indexCalculating()
+
 @app.route("/")
 def get_daily(): 
     return jsonify({"titles": get_articles_top().articles})
 
-@app.route("/by_link/<publisher>/<link>")
-def get_content_by_link(publisher, link):
+@app.route("/get_content/<article>")
+def get_content_by_link(article):
 
     # art = political_bias.get_articles_relavant().articles
     # return(political_bias.get_article_content(art[50]["url"], art[50]["media_name"]))
     #return(art[50].update({"content" : political_bias.get_article_content(art[50]["url"], art[50]["media_name"])}))
-    return(get_article_content(link, publisher))
+    return jsonify({"content" : get_article_content(article["url"], article["media_name"])})
     # for i in range(len(art)):
     #     if (art[i]["title"] == title):
     #         return art[i]
@@ -39,6 +45,11 @@ def get_similar_articles(publisher):
     return get_articles_relevant(publisher)
 
     #return(political_bias.get_similar_articles("https://www.cnn.com/2023/05/12/business/airbag-inflator-recall-arc/index.html", "CNN"))
+
+@app.route("/bias/<article>")
+def get_bias(article):
+    return jsonify({"bias": get_bias_from_article(article["url"], article["media_name"])})
+
 
 if __name__ == '__main__':
     app.run()
